926254b4ea882a8c2cea6af9957f990e3e91e97a
1damketthuc <longthtran@gmail.com>
2024-06-16 08:39:18 +0700

    Add simple test for user controller

--- src/main/java/org/example/config/WebSecurityConfig.java
+++ src/main/java/org/example/config/WebSecurityConfig.java
@@ -3,11 +3,19 @@ package org.example.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.Map;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -19,11 +27,22 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-          .authorizeHttpRequests((authz) -> authz
-            .requestMatchers("/api/auth/**", "/console").permitAll()
+          .authorizeHttpRequests((auth) -> auth
+            .requestMatchers(new AntPathRequestMatcher("/api/auth/**")).permitAll()
+            .requestMatchers(HttpMethod.GET, "/api/users").permitAll()
+            .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
+            .requestMatchers("/api/users/mem").hasRole("USER")
             .anyRequest().authenticated()
           )
-          .httpBasic(withDefaults());
+          .httpBasic(withDefaults())
+          .csrf(AbstractHttpConfigurer::disable)
+          .headers(headers ->
+            headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+          ).exceptionHandling(e -> e.authenticationEntryPoint((request, response, authException) -> {
+              response.setStatus(HttpStatus.UNAUTHORIZED.value());
+              response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+              response.getWriter().write(Map.of("error", "You are not authenticated!").toString());
+          }));
         return http.build();
     }
 
