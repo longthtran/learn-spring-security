8ba4b159363183a4b069f61a26984f11fca861a0
1damketthuc <longthtran@gmail.com>
2024-06-16 07:59:18 +0700

    Enable jpa auditing

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -15,9 +15,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/users")
 @AllArgsConstructor
 @Slf4j
 public class UserController {
